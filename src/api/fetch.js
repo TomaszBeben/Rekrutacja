5a50094b51375db527ece86eaaf74fee1290b672
TomaszBeben <tbeben@st.swps.edu.pl>
2021-11-12 02:34:51 +0100

    functionality works, tests passed, structure fixed v2

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -8,5 +8,17 @@ import axios from "axios";
       setState(res.data)
     }).catch(e => {
       console.error(e.message);
-    });
-  };
+    })
+  }
+
+// export const fetchData = async (url, setState) => {
+
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     setState(data)
+
+//   } catch(err) {
+//     console.error(err)
+//   }
+// }
